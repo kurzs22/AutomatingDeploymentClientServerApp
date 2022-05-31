cc6240953b875fbd33859d1f77cbdeea283da529
Stefan Kurz <kurzs@telekom.de>
2022-05-31 14:34:41 +0200

    Add Item model and migration

---
+++ data-layer/models/item.js
@@ -0,0 +1,16 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Item = sequelize.define(
+    'Item',
+    {
+      id: { type: DataTypes.STRING, primaryKey: true },
+      name: { type: DataTypes.STRING, allowNull: false },
+    },
+    {}
+  );
+  Item.associate = function (models) {
+    // associations can be defined here
+  };
+  return Item;
+};
+
